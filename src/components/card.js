d1e58e5cf837f2e1277d105a6f69d58d67553a92
Brian Thomas <btdeveloper125@gmail.com>
2023-01-22 11:32:00 -0700

    refactor(Dashboard Data):
    Added logic to how the cards have svgs/styles applied

--- src/components/card.js
+++ src/components/card.js
@@ -1,7 +1,43 @@
 import React from 'react';
 import {CardBackground, CardForeground, CardTitle, CardContentContainer, CardHeader, CardMenuDot, CardMenuDotContainer, CardBody, CardDataMain, CardDataSecondary} from '../styles/card'
+//Img
+import WorkIcon from '../images/icon-work.svg'
+import PlayIcon from '../images/icon-play.svg'
+import StudyIcon from '../images/icon-study.svg'
+import ExerciseIcon from '../images/icon-exercise.svg'
+import SocialIcon from '../images/icon-social.svg'
+import SelfIcon from '../images/icon-self-care.svg'
 
 const Card = ({data}) => {
+  let image, bg
+  const CardDesign = () =>{
+    // console.log(data.timeframes.weekly)
+    if(data.title === 'Work'){
+      image = WorkIcon
+      bg = 'work'
+    }
+    else if(data.title === 'Play'){
+      image = PlayIcon
+      bg = 'play'
+    }
+    else if(data.title === 'Study'){
+      image = StudyIcon
+      bg = 'study'
+    }
+    else if(data.title === 'Exercise'){
+      image = ExerciseIcon
+      bg = 'exercise'
+    }
+    else if(data.title === 'Social'){
+      image = SocialIcon
+      bg = 'social'
+    }
+    else if(data.title === 'Self Care'){
+      image = SelfIcon
+      bg = 'self'
+    }
+  }
+  CardDesign()
   return (
     <>
       <CardBackground style={
--- src/components/layout.js
+++ src/components/layout.js
@@ -10,11 +10,12 @@ const GlobalStyle = createGlobalStyle`
   }
   :root{
     --color-primary-blue: hsl(246, 80%, 60%);
-    --color-primary-red-work: hsl(15,100%,68%);
-    --color-primary-blue-play: hsl(195, 74%, 62%);
-    --color-primary-green-exercise: hsl(145, 58%, 55%);
-    --color-primary-violet-social: hsl(264, 64%, 52%);
-    --color-primary-orange--self: hsl(43, 84%, 65%);
+    --color-primary-work: hsl(15,100%,68%);
+    --color-primary-study: hsl(348, 100%, 68%);
+    --color-primary-play: hsl(195, 74%, 62%);
+    --color-primary-exercise: hsl(145, 58%, 55%);
+    --color-primary-social: hsl(264, 64%, 52%);
+    --color-primary-self: hsl(43, 84%, 65%);
     --color-neutral-dark-blue1: hsl(226, 43%, 10%);
     --color-neutral-dark-blue2: hsl(235, 46%, 20%);
     --color-neutral-blue: hsl(235, 45%, 61%);
