0f69286bb945584cf70fefc6b558f749ea854db8
Brian Thomas <btdeveloper125@gmail.com>
2023-01-22 11:28:08 -0700

    feat(JSON): Added gatsby-transformer-sharp
    JSON data file will not be read in Graphql without this gatsby plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@ module.exports = {
     image: ``,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-transformer-remark", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-transformer-remark", "gatsby-plugin-sharp", "gatsby-transformer-sharp","gatsby-transformer-json", {
     resolve: 'gatsby-source-filesystem',
     options: {
       "name": "images",
@@ -22,5 +22,13 @@ module.exports = {
       "path": "./src/pages/"
     },
     __key: "pages"
+  },{
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "data",
+      "path": `${__dirname}/src/data`
+    },
+    __key: "data"
+  
   }]
 };
